30eda1a080dc2c8388a87ce39b2802c0e9f021aa
artem <jock.fromrussia@gmail.com>
2026-01-19 10:47:28 +0300

    Made hero section and fixed bag in the mobile section

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const mobile = document.querySelector('.header__mobile');
-const mobileNav = document.querySelector('.mobile__nav');
-const title = document.querySelector('.header__title');
+const headerBurger = document.querySelector('.header__burger');
+const mobileBurger = document.querySelector('.mobile__burger');
+const mobile = document.querySelector('.mobile');
+const mobileLinks = document.querySelectorAll('.mobile__nav-link');
 
-mobile.addEventListener('click', () => {
-  mobile.classList.toggle('header__mobile--active');
-  mobileNav.classList.toggle('mobile__nav--active');
-  title.classList.toggle('header__title--active');
+headerBurger.addEventListener('click', () => {
+  headerBurger.classList.toggle('burger--clicked');
+  mobile.classList.toggle('mobile--opened');
+});
+
+mobileBurger.addEventListener('click', () => {
+  headerBurger.classList.toggle('burger--clicked');
+  mobile.classList.toggle('mobile--opened');
+});
+
+mobileLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    headerBurger.classList.toggle('burger--clicked');
+    mobile.classList.toggle('mobile--opened');
+  });
 });
