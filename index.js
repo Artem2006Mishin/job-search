c17ad973b4e8958d6889e6b948d2b13589967d3e
artem <jock.fromrussia@gmail.com>
2026-03-17 12:59:45 +0300

    Fix categories__list, create category-card. Rename google-icon to google. Fix header and hero__petals.Split decor on lines and ovals. Fix posts__list. And fix registration section.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const burger = document.querySelector(".header__burger");
 const title = document.querySelector(".header__title");
 const menu = document.querySelector(".mobile-menu");
 const links = document.querySelectorAll(".mobile-menu__link");
+const header = document.querySelector(".header");
 const body = document.body;
 
 function toggleMenu() {
@@ -9,6 +10,7 @@ function toggleMenu() {
   title.classList.toggle("header__title--white");
   menu.classList.toggle("mobile-menu--opened");
   burger.classList.toggle("burger--clicked");
+  header.classList.toggle("header--transparent");
 }
 
 burger.addEventListener("click", toggleMenu);
